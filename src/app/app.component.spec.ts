2e83ca82ea93cd27670e2dfe4bd020d55fea6e6a
sndstudy <sanada2054tech@gmail.com>
2018-05-02 02:12:08 +0900

    AngularCLIで出力されたテストコードがパスするように修正

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,41 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { 
+  MatExpansionModule,
+  MatToolbarModule,
+  MatRadioModule 
+} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { MemberlistComponent } from './memberlist/memberlist.component';
+import { MemberlistService } from './service/memberlist.service'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MemberlistComponent
       ],
+      imports: [
+        MatExpansionModule,
+        MatToolbarModule,
+        MatRadioModule,
+        FormsModule, 
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        BrowserModule
+      ],
+      providers: [MemberlistService]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- src/app/memberlist/memberlist.component.spec.ts
+++ src/app/memberlist/memberlist.component.spec.ts
@@ -2,13 +2,35 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MemberlistComponent } from './memberlist.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { 
+  MatExpansionModule,
+  MatToolbarModule,
+  MatRadioModule 
+} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberlistService } from '../service/memberlist.service'
+
 describe('MemberlistComponent', () => {
   let component: MemberlistComponent;
   let fixture: ComponentFixture<MemberlistComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MemberlistComponent ]
+      declarations: [ MemberlistComponent ],
+      imports: [
+        MatExpansionModule,
+        MatToolbarModule,
+        MatRadioModule,
+        FormsModule, 
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        BrowserModule
+      ],
+      providers: [MemberlistService]
     })
     .compileComponents();
   }));
